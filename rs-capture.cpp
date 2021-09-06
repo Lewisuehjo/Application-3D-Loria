a0f927b87d3dc0f0e30ed7378a80a63c75c4d5c6
Julien <julien.deviller@epitech.eu>
2021-09-06 10:58:25 +0200

    adding requirements lib

---
+++ rs-capture.cpp
@@ -0,0 +1,42 @@
+﻿// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include "example.hpp"          // Include short list of convenience functions for rendering
+
+
+int main(int argc, char * argv[]) try
+{
+    rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
+    // Create a simple OpenGL window for rendering:
+    window app(1280, 720, "Application Capture");
+
+    rs2::colorizer color_map;
+    rs2::rates_printer printer;
+
+    rs2::pipeline pipe;
+
+    pipe.start();
+
+    while (app)
+    {
+        rs2::frameset data = pipe.wait_for_frames().    // Wait for next set of frames from the camera
+                             apply_filter(printer).     // Print each enabled stream frame rate
+                             apply_filter(color_map);   // Find and colorize the depth data
+
+        // The show method, when applied on frameset, break it to frames and upload each frame into a gl textures
+        // Each texture is displayed on different viewport according to it's stream unique id
+        app.show(data);
+    }
+    return EXIT_SUCCESS;
+}
+catch (const rs2::error & e)
+{
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
